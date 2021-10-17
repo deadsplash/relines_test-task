8adee3b611f134e5482854794b0ed5db7c75692c
deadsplash <deadsplashmail@gmail.com>
2021-10-17 19:19:55 +0300

    wrapped code into the script
    improved code functionality

--- main.py
+++ main.py
@@ -2,19 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 
 
-url = 'https://habr.com/ru/'
-
-#print(soup.find('main').text.strip())
-
-#   habr и howtogeek держат главное в h2
-# find_all_articles = soup.find('main').findAll('h2') # howtogeek.com personally
-# find_all_articles = soup.findAll('h2') # работает с обоими
-
-# find_all_articles = soup.find('div').findAll('a')  # hacker.ru
-
-# find_all_articles = soup.find('div').findAll('span')  # https://dtf.ru/
-
-# print(find_all_articles)   # for debug needs
+url = 'https://xakep.ru/'
 
 
 def parser(url, save_img=False, save_to_txt=False):
@@ -31,24 +19,39 @@ def parser(url, save_img=False, save_to_txt=False):
     soup = BeautifulSoup(r, 'lxml')
 
     find_all_articles = soup.findAll('article')
+    finder = 'span'
+
+    if len(find_all_articles) < 2:
+        print('Unable to parse this URL')
+        exit()
+
+    if len(find_all_articles[1].find(finder).text) < 30:
+        finder = 'h2'
+    else:
+        print(len(find_all_articles))
+        pass
 
     #   used for article type HTML
     for i in range(len(find_all_articles)):
 
         try:
 
-            item = find_all_articles[i].find('h2').text
+            # ищем названия статей по странице
+            item = find_all_articles[i].find(finder).text
             print(item)
             save[item] = ""
 
             if save_img is True and len(item) > 5:
                 try:
+                    # чистим ссылку от всего лишнего
                     pic = str(find_all_articles[i].find('img').get('src')).replace('//', '').replace('https:', '')
                     print(pic + '\n' + '\n')
+                    # делаем человеческие отступы
                     save[item] = pic + '\n'
                 except:
                     pass
-        except:
+
+        except Exception:
             pass
 
     if save_to_txt is True:
@@ -57,4 +60,33 @@ def parser(url, save_img=False, save_to_txt=False):
                 f.write(i + '\n' + save[i] + '\n')
 
 
-parser(url, save_img=False, save_to_txt=True)
+def main():
+
+    print("URL: ")
+    url = str(input())
+
+    print('Do you want to collect IMG links? Y/N')
+    checker = str(input()).upper()
+    if checker == 'Y':
+        save_img = True
+    elif checker == 'N':
+        save_img = False
+    else:
+        save_img = False
+        print('Wrong input, script will not collect IMG links.')
+
+    print('Do you want to save your data into TXT file? Y/N')
+    checker = str(input()).upper()
+    if checker == 'Y':
+        save_to_txt = True
+    elif checker == 'N':
+        save_to_txt = False
+    else:
+        save_to_txt = False
+        print('Wrong input, script will not save into text.')
+
+    parser(url, save_img=save_img, save_to_txt=save_to_txt)
+
+
+if __name__ == '__main__':
+    main()
